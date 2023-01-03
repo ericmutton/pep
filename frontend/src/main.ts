ce498a40eb947674db1ff2039e6477371556878a
ericmutton <64441308+ericmutton@users.noreply.github.com>
2023-01-02 23:05:21 -0600

    Prepare frontend files for dockerization

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import MarkdownIt from 'markdown-it'
 
 import './assets/main.css'
+
+// Markdown renderer Vue Composable
+var markdown = new MarkdownIt();
+export function md(input: string) {
+    return markdown.render(input);
+}
 
 const app = createApp(App)
 
