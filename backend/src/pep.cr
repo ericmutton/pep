431c4b75575fc919c786fe27bf812aec24267503
kotahu <me@kotahu.com>
2023-01-05 21:21:08 -0600

    add development database to compose

--- backend/src/pep.cr
+++ backend/src/pep.cr
@@ -1,11 +1,17 @@
 # TODO: Write documentation for `Pep`
 require "kemal"
+require "db"
+require "pg"
+
+db = DB.open("postgresql://pep:admin@database:5432/pep")
 module Pep
   VERSION = "0.1.0"
 
   # TODO: Put your code here
-  get "/" do
-    "Hello World!"
+  get "/" do |env|
+    resp = db.query_one("SELECT NOW()", as: {Time})
+    env.response.content_type = "application/json"
+    {"time": resp}.to_json 
   end
 
   Kemal.run
