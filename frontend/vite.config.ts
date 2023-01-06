d3568b2c6470354f0997064fc7f8a2b7caebf261
kotahu <me@kotahu.com>
2023-01-06 12:16:31 -0600

    frontend in docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
       port: 5173,
       proxy: {
           '/V1': {
-              target: 'http://localhost:3000',
+              target: 'http://backend:3000',
               changeOrigin: true,
               secure: false,
               rewrite: (path) => path.replace(/^\/V1/, '')
