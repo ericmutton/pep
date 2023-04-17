7fca082e117db5e16bbfcbcd585d9db5cf8a796e
ericmutton <64441308+ericmutton@users.noreply.github.com>
2023-04-16 21:41:15 -0500

    Initial Docus restructure

---
+++ frontend/docs/app.config.ts
@@ -0,0 +1,23 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Explore',
+    socials: {
+      github: 'ericmutton/pep'
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      logo: true
+    },
+    footer: {
+      iconLinks: [
+        {
+          href: 'https://nuxt.com',
+          icon: 'simple-icons:nuxtdotjs'
+        }
+      ]
+    }
+  }
+})
---
+++ frontend/docs/nuxt.config.ts
@@ -0,0 +1,6 @@
+export default defineNuxtConfig({
+  extends: '@nuxt-themes/docus',
+  // plugins: [
+  //   { src: '@/plugins/vue-codemirror', ssr: false }
+  // ]
+})
